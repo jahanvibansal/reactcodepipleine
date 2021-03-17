0d46ef03ba557158a67715b228e5acc99623c3df
Payal Bansal <jahanvi.bansal@gmail.com>
2021-03-17 17:01:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Modified <code>src/App.js</code> and checking webhook.
         </p>
         <a
           className="App-link"
