e0a6fa7fd1e427c9ed41607134e836450fe0f3a6
Payal Bansal <jahanvi.bansal@gmail.com>
2021-03-18 12:44:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Checking Dev <code>src/App.js</code> and save to reload.
+          test Checking Dev <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
